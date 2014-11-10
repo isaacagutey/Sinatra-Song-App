26484ad1261d36631f7821158b98a13a846e72de
isaacagutey <isaacagutey@gmail.com>
2014-11-10 18:06:33 -0500

    somechanges

--- main.rb
+++ main.rb
@@ -5,6 +5,13 @@ require 'sass'
 #require 'sinatra/reloader' if development?
 
 
+configure :production do
+  DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/development.db")
+end
+
+configure :production do
+  DataMapper.setup(:default, ENV['DATABASE_URL'])
+end
 
 configure do
   enable :sessions
@@ -79,3 +86,43 @@ get '/logout' do
   redirect to('/login')
 end
 
+############################################
+
+
+get '/songs' do
+  halt(401,'Not Authorized') unless session[:admin]
+  @songs=Song.all
+  slim :songs
+end
+
+get '/songs/new' do
+  @song = Song.new
+  slim :new_song
+end
+
+get '/songs/:id' do
+  @song = Song.get(params[:id])
+  slim :show_song
+end
+
+post '/songs' do
+  song=Song.create(params[:song])
+  redirect to("/songs/#{song.id}")
+
+end
+
+get '/songs/:id/edit' do
+  @song = Song.get(params[:id])
+  slim :edit_song
+end
+
+put '/songs/:id' do
+  song=Song.get(params[:id])
+  song.update(params[:song])
+  redirect to("/songs/#{song.id}")
+end
+
+delete '/songs/:id' do
+  Song.get(params[:id]).destroy
+  redirect to('/songs')
+end
--- song.rb
+++ song.rb
@@ -2,15 +2,6 @@ require './main.rb'
 require 'dm-core'
 require 'dm-migrations'
 
-
-configure :production do
-  DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/development.db")
-end
-
-configure :production do
-  DataMapper.setup(:default, ENV['DATABASE_URL'])
-end
-
 class Song
   include DataMapper::Resource
   property :id, Serial
@@ -25,41 +16,3 @@ class Song
 end
 
 DataMapper.finalize
-
-get '/songs' do
-  halt(401,'Not Authorized') unless session[:admin]
-  @songs=Song.all
-  slim :songs
-end
-
-get '/songs/new' do
-  @song = Song.new
-  slim :new_song
-end
-
-get '/songs/:id' do
-  @song = Song.get(params[:id])
-  slim :show_song
-end
-
-post '/songs' do
-  song=Song.create(params[:song])
-  redirect to("/songs/#{song.id}")
-
-end
-
-get '/songs/:id/edit' do
-  @song = Song.get(params[:id])
-  slim :edit_song
-end
-
-put '/songs/:id' do
-  song=Song.get(params[:id])
-  song.update(params[:song])
-  redirect to("/songs/#{song.id}")
-end
-
-delete '/songs/:id' do
-  Song.get(params[:id]).destroy
-  redirect to('/songs')
-end
