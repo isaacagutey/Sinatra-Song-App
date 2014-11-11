64c6e8bf64cb61a292f827470cee1441575d90c7
isaacagutey <isaacagutey@gmail.com>
2014-11-11 11:23:27 -0500

    updated

--- main.rb
+++ main.rb
@@ -2,10 +2,10 @@ require './song.rb'
 require 'sinatra'
 require 'slim'
 require 'sass'
-#require 'sinatra/reloader' if development?
+require 'data_mapper'
+require 'sinatra/reloader' if development?
 
-
-configure :production do
+configure :development do
   DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/development.db")
 end
 
@@ -18,6 +18,15 @@ configure do
   set :username, 'isaac'
   set :password, 'agutey'
 end
+
+helpers do
+  def css(*stylesheets)
+    stylesheets.map do |stylesheet|
+      "<link href=\"/#{stylesheet}.css\" media=\"screen, projection\"rel=\"stylesheet\" />"
+      end.join
+    end
+  end
+
 
 get('/styles.css'){scss :styles}
 
@@ -50,17 +59,17 @@ get '/fake-error' do
   'There’s nothing wrong, really :P'
 end
 
-# get '/environment' do
-#   if development?
-#     "development"
-#   elsif production?
-#     "production"
-#   elsif test?
-#     "test"
-#   else
-#     "who cares about the environment anyway"
-#   end
-# end
+get '/environment' do
+  if development?
+    "development"
+  elsif production?
+    "production"
+  elsif test?
+    "test"
+  else
+    "who cares about the environment anyway"
+  end
+end
 
 get '/set/:name' do
   halt(401,'Not Authorized') unless session[:admin]
@@ -86,43 +95,3 @@ get '/logout' do
   redirect to('/login')
 end
 
-############################################
-
-
-get '/songs' do
-  halt(401,'Not Authorized') unless session[:admin]
-  @songs=Song.all
-  slim :songs
-end
-
-get '/songs/new' do
-  @song = Song.new
-  slim :new_song
-end
-
-get '/songs/:id' do
-  @song = Song.get(params[:id])
-  slim :show_song
-end
-
-post '/songs' do
-  song=Song.create(params[:song])
-  redirect to("/songs/#{song.id}")
-
-end
-
-get '/songs/:id/edit' do
-  @song = Song.get(params[:id])
-  slim :edit_song
-end
-
-put '/songs/:id' do
-  song=Song.get(params[:id])
-  song.update(params[:song])
-  redirect to("/songs/#{song.id}")
-end
-
-delete '/songs/:id' do
-  Song.get(params[:id]).destroy
-  redirect to('/songs')
-end
